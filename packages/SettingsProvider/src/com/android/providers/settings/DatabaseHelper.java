b5ba663d1a04f908d785a4658ecf58e7ca331f0e
fuzz <fuzzzzzzzzzzzzzzzzzz@gmail.com>
2013-03-29 12:44:49 +0900

    SettingsProvider: Make default value of Status bar brightness control overlayable
    also add missing quick pulldown

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -1980,6 +1980,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
             loadIntegerSetting(stmt, Settings.System.STATUS_BAR_NOTIF_COUNT,
                     R.integer.def_notif_count);
+
+            loadIntegerSetting(stmt, Settings.System.QS_QUICK_PULLDOWN,
+                    R.integer.def_qs_quick_pulldown);
         } finally {
             if (stmt != null) stmt.close();
         }
